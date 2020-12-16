2b87d52739a455a4dc64cb66befa04b15cf90696
samhooper256 <samhooper256@gmail.com>
2020-12-16 09:39:23 -0600

    Improved day16 solution; fixed bug in HashIntSet

--- AdventOfCode2020/src/day16/Solution.java
+++ AdventOfCode2020/src/day16/Solution.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import utils.*;
 
 /**
+ * Correct answers: 20048 (Part 1) and 4810284647569 (Part 2)
  * @author Sam Hooper
  *
  */
@@ -56,14 +57,14 @@ public class Solution {
 	}
 	
 	private static void solvePart2(String[] sections) {
-		Map<String, Set<Integer>> indicesSatisfying = new HashMap<>();
+		Map<String, IntSet> indicesSatisfying = new HashMap<>();
 		List<int[]> allTicketsAsList = allTickets(sections);
 		removeInvalidTickets(allTicketsAsList);
 		int[][] allTickets = allTicketsAsList.toArray(int[][]::new);
 		for(Map.Entry<String, IntPredicate> entry : validationFunctions.entrySet()) {
 			String name = entry.getKey();
 			IntPredicate func = entry.getValue();
-			HashSet<Integer> set = new HashSet<>(20);
+			IntSet set = IntSet.createHash(20);
 			outer1:
 			for(int col = 0; col < allTickets[0].length; col++) {
 				for(int row = 0; row < allTickets.length; row++)
@@ -73,16 +74,16 @@ public class Solution {
 			}
 			indicesSatisfying.put(name, set);
 		}
-		indicesSatisfying.forEach((name, set) -> System.out.printf("%s : %s%n", name, set));
+//		indicesSatisfying.forEach((name, set) -> System./out.printf("%s : %s%n", name, set));
 		
 		Map<String, Integer> correctIndices = new HashMap<>();
 		outer2:
 		while(!indicesSatisfying.isEmpty()) {
-			System.out.printf("indicesSatisfying=%s%n", indicesSatisfying);
-			for(Iterator<Entry<String, Set<Integer>>> iterator = indicesSatisfying.entrySet().iterator(); iterator.hasNext();) {
-				Map.Entry<String, Set<Integer>> entry = iterator.next();
+//			System.out.printf("indicesSatisfying=%s%n", indicesSatisfying);
+			for(Iterator<Entry<String, IntSet>> iterator = indicesSatisfying.entrySet().iterator(); iterator.hasNext();) {
+				Map.Entry<String, IntSet> entry = iterator.next();
 				Set<Integer> set = entry.getValue();
-				System.out.printf("\tentry=%s%n", entry);
+//				System.out.printf("\tentry=%s%n", entry);
 				if(set.size() == 1) {
 					int singleValue = set.iterator().next();
 					for(Set<Integer> otherSet : indicesSatisfying.values()) {
@@ -95,7 +96,7 @@ public class Solution {
 			}
 			throw new IllegalStateException("Shouldn't get here");
 		}
-		System.out.println("correctIndices="+correctIndices);
+//		System.out.println("correctIndices="+correctIndices);
 		int[] myTicket = allTickets[0];
 		long product = 1;
 		for(var e : correctIndices.entrySet()) {
--- AdventOfCode2020/src/utils/HashIntSet.java
+++ AdventOfCode2020/src/utils/HashIntSet.java
@@ -38,7 +38,23 @@ public class HashIntSet implements IntSet {
 		}
 		
 		/**
-		 * {@inheritDoc} Returns {@code null} if {@code (!hasNext())}.
+		 * Returns the number of {@link Node Nodes} that come after this one, including {@code this}.
+		 * {@code lengthInclusive()} is equal to {@code (lengthExclusive() + 1)}.
+		 */
+		public int lengthInclusive() {
+			return lengthExclusive() + 1;
+		}
+		
+		/**
+		 * Returns the number of {@link Node Nodes} that come {@link #next() after} this one, excluding {@code this}.
+		 * If {@link #hasNext()} is {@code false}, returns {@code 0}.
+		 */
+		public int lengthExclusive() {
+			return hasNext() ? next().lengthInclusive() : 0;
+		}
+		
+		/**
+		 * Returns the {@link Node} that comes immediately after this one, or {@code null} if {@code (!hasNext())}.
 		 */
 		@Override
 		public Node next() {
@@ -142,23 +158,47 @@ public class HashIntSet implements IntSet {
 		return bucketFor(val, this.buckets);
 	}
 	
+	/** Returns the first {@link Node} in the {@link #buckets bucket} that the given {@code int} value ({@code val}) should be put in. Returns
+	 * {@code null} if that bucket is empty.*/
 	private static Node bucketFor(final int val, final Node[] buckets) {
 		return buckets[bucketIndex(val, buckets)];
 	}
 	
+	/** Returns the index of the {@link #buckets bucket} that the given {@code int} value should be put in. */
 	private int bucketIndex(final int val) {
 		return bucketIndex(val, this.buckets);
 	}
 	
+	/** Returns the index in {@code buckets} of the bucket that the given {@code int} value should be put in. */
 	private static int bucketIndex(final int val, final Node[] buckets) {
 		return Math.abs(val) % buckets.length;
 	}
 	
+	/** Clears the {@link #buckets bucket} at {@code bucketIndex}. Updates {@link #size} accordingly. */
+	private void clearBucket(final int bucketIndex) {
+		Node bucket = buckets[bucketIndex];
+		clearBucket(bucketIndex, buckets);
+		size -= bucket.lengthInclusive();
+	}
+	
+	/** Sets {@code buckets[bucketIndex]} to {@code null}.*/
+	private static void clearBucket(final int bucketIndex, final Node[] buckets) {
+		buckets[bucketIndex] = null;
+	}
+	
 	@Override
 	public boolean remove(final int val) {
-		Node bucket = bucketFor(val);
-		if(!bucket.hasNext())
+		int bucketIndex = bucketIndex(val);
+		Node bucket = buckets[bucketIndex];
+		if(bucket == null)
 			return false;
+		if(!bucket.hasNext()) {
+			if(bucket.value() == val) {
+				clearBucket(bucketIndex);
+				return true;
+			}
+			return false;
+		}
 		for(Node temp = bucket.next(); bucket.hasNext(); bucket = bucket.next(), temp = bucket.next()) {
 			if(temp.value() == val) {
 				bucket.removeNext();
@@ -256,7 +296,6 @@ public class HashIntSet implements IntSet {
 				}
 			}
 			
-			
 		};
 	}
 
@@ -277,8 +316,6 @@ public class HashIntSet implements IntSet {
 			public void remove() {
 				throw new NoSuchElementException();
 			}
-			
-			
 			
 		};
 	}
@@ -313,7 +350,9 @@ public class HashIntSet implements IntSet {
 
 	@Override
 	public boolean remove(Object o) {
-		throw new UnsupportedOperationException(); //TODO
+		if(o.getClass() == Integer.class)
+			return remove(((Integer) o).intValue());
+		return false;
 	}
 
 	@Override
