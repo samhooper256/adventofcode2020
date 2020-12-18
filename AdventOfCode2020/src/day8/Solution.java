3b903b2c3bfc58469ebd09b77605b9ac5e8c3e51
samhooper256 <samhooper256@gmail.com>
2020-12-17 21:10:40 -0600

    Improved day8 code.

--- AdventOfCode2020/src/day8/Solution.java
+++ AdventOfCode2020/src/day8/Solution.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import utils.*;
 
 /**
+ * Correct answers are 1610 (Part 1) and 1703 (Part 2).
  * @author Sam Hooper
  *
  */
@@ -17,8 +18,8 @@ public class Solution {
 	}
 	
 	private static class Solver {
-		private final String[] input;
 		
+		private final String[] input;
 		private long accumulator = 0;
 		
 		public Solver(final String[] input) {
@@ -29,6 +30,7 @@ public class Solution {
 			System.out.println(getResult().second());
 		}
 		
+		/** The {@link Boolean} in the {@link Pair} is {@code true} if the program terminated, {@code false} otherwise. */
 		private Pair<Boolean, Long> getResult() {
 			accumulator = 0;
 			int nextInstruction = 0;
@@ -43,26 +45,19 @@ public class Solution {
 		}
 		
 		public void solvePart2() {
+			terminatingValueAfterBeingFixed();
+		}
+
+		private long terminatingValueAfterBeingFixed() {
 			for(int i = 0; i < input.length; i++) {
 				final String line = input[i];
-				if(line.charAt(0) == 'n') {
-					input[i] = "jmp" + input[i].substring(3);
-					Pair<Boolean, Long> res = getResult();
-					input[i] = line;
-					if(res.first()) {
-						System.out.println(res.second());
-						return;
-					}
-				}
-				else if(line.charAt(0) == 'j') {
-					input[i] = "nop" + input[i].substring(3);
-					Pair<Boolean, Long> res = getResult();
-					input[i] = line;
-					if(res.first()) {
-						System.out.println(res.second());
-						return;
-					}
-				}
+				if(line.charAt(0) != 'n' && line.charAt(0) != 'j')
+					continue;
+				input[i] = (line.startsWith("j") ? "nop" : "jmp") + input[i].substring(3);
+				Pair<Boolean, Long> res = getResult();
+				input[i] = line;
+				if(res.first())
+					return res.second();
 			}
 			throw new IllegalStateException("Could not find the correct instruction to modify");
 		}
@@ -71,16 +66,15 @@ public class Solution {
 		 * Returns the index in {@link #input} of the next instruction to run.
 		 */
 		private int exec(final int instruction) {
-			String str = input[instruction];
-			final String[] split = str.split(" ", 2);
+			final String[] split = input[instruction].split(" ", 2);
 			return switch(split[0]) {
-			case "nop" -> instruction + 1;
-			case "acc" -> {
-				accumulator += Integer.parseInt(split[1]);
-				yield instruction + 1;
-			}
-			case "jmp" -> instruction + Integer.parseInt(split[1]);
-			default -> throw new IllegalStateException("Unknown instruction: \"" + str + "\"");
+				case "nop" -> instruction + 1;
+				case "acc" -> {
+					accumulator += Integer.parseInt(split[1]);
+					yield instruction + 1;
+				}
+				case "jmp" -> instruction + Integer.parseInt(split[1]);
+				default -> throw new IllegalStateException("Unknown instruction: \"" + input[instruction] + "\"");
 			};
 		}
 	}
